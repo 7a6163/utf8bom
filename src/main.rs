ee41412460d6aa1525648010bb33be3106de4507
Zac <579103+7a6163@users.noreply.github.com>
2023-09-21 11:37:29 +0800

    refactor: Refactor file handling in main.rs
    - Remove unused imports for `BufWriter` and `BufReader`
    - Replace UTF-8 BOM check with byte comparison check

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,30 @@
 use std::env;
 use std::fs::File;
-use std::io::{self, BufReader, BufWriter, Read, Write};
+use std::io::{self, Read, Write};
 use std::process;
 
+const UTF8_BOM: [u8; 3] = [0xEF, 0xBB, 0xBF];
+
 fn add_utf8_bom(input_path: &str, output_path: &str) -> io::Result<()> {
-    let input_file = File::open(input_path)?;
-    let mut reader = BufReader::new(input_file);
+    let mut input_file = File::open(input_path)?;
+    let mut content = Vec::new();
+    input_file.read_to_end(&mut content)?;
 
-    let output_file = File::create(output_path)?;
-    let mut writer = BufWriter::new(output_file);
+    let has_bom = content.starts_with(&UTF8_BOM);
 
-    // Read the first 3 bytes
-    let mut bom = [0u8; 3];
-    let _ = reader.read_exact(&mut bom);
-
-    if bom != [0xEF, 0xBB, 0xBF] {
-        // Not UTF-8 BOM present in the input file
-        print!("Input file does not have UTF-8 BOM.");
-        return Ok(())
+    if has_bom {
+        println!("The input file already has UTF-8 BOM.");
+        return Ok(());
     }
 
+    let output_file = File::create(output_path)?;
+    let mut writer = output_file;
+
     // Write the UTF-8 BOM
-    writer.write_all(&[0xEF, 0xBB, 0xBF])?;
+    writer.write_all(&UTF8_BOM)?;
 
     // Write the remaining content
-    io::copy(&mut reader, &mut writer)?;
+    writer.write_all(&content)?;
 
     Ok(())
 }
